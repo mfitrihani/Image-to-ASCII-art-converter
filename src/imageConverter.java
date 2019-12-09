3a3840bb907c4c93070cb83e5a21d9c1b9b29f6a
mfitrihani <mfitrihani@gmail.com>
2019-12-09 15:34:32 +0800

    restructure code, add resizeImage method

--- src/imageConverter.java
+++ src/imageConverter.java
@@ -3,10 +3,12 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.lang.Math;
 
 public class imageConverter {
-    private BufferedImage image = null;
+    private BufferedImage image;
+    private Color[][] imageRGB = null;
+    private int[][] imageBrightness = null;
+    private String[][] ASCIIArt = null;
     private String[] ASCII =
             {"`", "^", ",", ":", ";", "I", "l", "!", "i",
                     "~", "+", "_", "-", "?", "]", "[", "}", "{", "1",
@@ -17,52 +19,83 @@ public class imageConverter {
                     "&", "8", "%", "B", "@", "$"};
 
     public imageConverter(String imagePath) throws IOException {
-        image = ImageIO.read(new File(imagePath)); //set buffered image
+        image = ImageIO.read(new File(imagePath));
     }
 
-    public Color[][] getImageRGBTuple() {
-        Color[][] imageTuple = new Color[image.getWidth()][image.getHeight()]; //create an array with size according to image size
+    public void resizeImage(){
+        BufferedImage newImage = new BufferedImage(image.getWidth()/5, image.getHeight()/5, BufferedImage.TYPE_INT_RGB);
+        Graphics g = newImage.createGraphics();
+        g.drawImage(image, 0, 0, image.getWidth()/5, image.getHeight()/5, null);
+        g.dispose();
+        image = newImage;
+    }
 
-        //get image RGB value and store them the array
+    public Color[][] getImageRGB() {
+        //check if imageRGB has value
+        if (imageRGB==null)
+            generateImageRGB();
+        return imageRGB;
+    }
+
+    public void generateImageRGB(){
+        //get image RGB value and store them in imageRGB array
+        imageRGB = new Color[image.getWidth()][image.getHeight()];
         for (int x = 0; x < image.getWidth(); x++) {
             for (int y = 0; y < image.getHeight(); y++) {
-                imageTuple[x][y] = new Color(image.getRGB(x, y));
+                imageRGB[x][y] = new Color(image.getRGB(x, y));
             }
         }
-
-        return imageTuple;
     }
 
     public int[][] getImageBrightness() {
-        Color[][] imageRGBTuple = getImageRGBTuple();
-        int[][] pixelBrightness = new int[image.getWidth()][image.getHeight()];
+        //check if imageBrightness has value
+        if (imageBrightness == null)
+            generateImageBrightness();
+        return imageBrightness;
+    }
 
+    public void generateImageBrightness(){
         //get brightness by getting average of RGB Value
+        imageBrightness = new int[image.getWidth()][image.getHeight()];
+        Color[][] rgbValue = getImageRGB();
         for (int x = 0; x < image.getWidth(); x++) {
             for (int y = 0; y < image.getHeight(); y++) {
-                pixelBrightness[x][y] = (imageRGBTuple[x][y].getRed() + imageRGBTuple[x][y].getGreen() + imageRGBTuple[x][y].getBlue()) / 3;
+                imageBrightness[x][y] = (rgbValue[x][y].getRed() + rgbValue[x][y].getGreen() + rgbValue[x][y].getBlue()) / 3;
             }
         }
-        return pixelBrightness;
     }
 
     public String[][] getImageASCIIArt() {
-        String[][] convertedArt = new String[image.getWidth()][image.getHeight()];
+        //check if ASCIIArt has value
+        if (ASCIIArt==null)
+            generateImageASCIIArt();
+        return ASCIIArt;
+    }
+
+    public void generateImageASCIIArt(){
+        ASCIIArt = new String[image.getWidth()][image.getHeight()];
         int[][] imageBrightness = getImageBrightness();
-        double divider = (double)ASCII.length/(double)255;;
+        double divider = (double)ASCII.length/(double)255;
         for (int x = 0; x < image.getWidth(); x++) {
             for (int y = 0; y < image.getHeight(); y++) {
                 if ((imageBrightness[x][y]*divider)%1>0.5){
-                    convertedArt[x][y] = ASCII[(int) Math.ceil(imageBrightness[x][y]*divider)];
+                    ASCIIArt[x][y] = String.format("%1$s%1$s%1$s",ASCII[(int) Math.ceil(imageBrightness[x][y]*divider)]);
                 }
                 else
-                    convertedArt[x][y] = ASCII[(int) Math.floor(imageBrightness[x][y]*divider)];
+                    ASCIIArt[x][y] = String.format("%1$s%1$s%1$s",ASCII[(int) Math.floor(imageBrightness[x][y]*divider)]);
             }
         }
-
-
-        return convertedArt;
     }
+
+//    public void invertImageBrightness() {
+//        int[][] brightness = getImageBrightness();
+//        //invert brightness value
+//        for (int x = 0; x < image.getWidth(); x++) {
+//            for (int y = 0; y < image.getHeight(); y++) {
+//                imageBrightness[x][y] = 255 - brightness[x][y];
+//            }
+//        }
+//    }
 
     public int getImageHeight() {
         return image.getHeight();
--- src/main.java
+++ src/main.java
@@ -1,15 +1,14 @@
-import java.awt.*;
 import java.io.IOException;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        imageConverter test = new imageConverter(".idea//Resources//ascii-pineapple.jpg");
+        imageConverter test = new imageConverter(".idea/Resources/ascii-pineapple.jpg");
 
         System.out.println("Image Height: " + test.getImageHeight() + " Image Width: " + test.getImageWidth());
         System.out.println("ASCII art: ");
 
+        test.resizeImage();
         String[][] ascii = test.getImageASCIIArt();
-
         for (int y = 0; y < test.getImageHeight(); y++) {
             for (int x = 0; x < test.getImageWidth(); x++) {
                 System.out.print(ascii[x][y]);
