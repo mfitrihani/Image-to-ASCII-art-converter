f57b19cdb222c983c73b03d0c5dba8974246dc00
mfitrihani <mfitrihani@gmail.com>
2019-12-09 12:19:30 +0800

    implement getImageASCIIArt method ,add new image resource

--- src/imageConverter.java
+++ src/imageConverter.java
@@ -3,17 +3,18 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.lang.Math;
 
 public class imageConverter {
     private BufferedImage image = null;
     private String[] ASCII =
-            {"`","^","\\",",",":",";","I","l","!", "i",
-                    "~","+","_","-","?","]","[","}","{","1",
-                    ")","(", "|","\\\\","/","t","f","j",":","r"
-                    ,"x","n","u","v","c", "z","X", "Y","U","J",
-                    "C","L","Q","0","O","Z","m","w", "q","p","d"
-                    ,"b","k", "h","a","o","*","#","M","W","&",
-                    "8","%","B","@","$"};
+            {"`", "^", ",", ":", ";", "I", "l", "!", "i",
+                    "~", "+", "_", "-", "?", "]", "[", "}", "{", "1",
+                    ")", "(", "|", "/", "t", "f", "j", ":", "r",
+                    "x", "n", "u", "v", "c", "z", "X", "Y", "U", "J",
+                    "C", "L", "Q", "0", "O", "Z", "m", "w", "q", "p",
+                    "d" , "b", "k", "h", "a", "o", "*", "#", "M", "W",
+                    "&", "8", "%", "B", "@", "$"};
 
     public imageConverter(String imagePath) throws IOException {
         image = ImageIO.read(new File(imagePath)); //set buffered image
@@ -23,9 +24,9 @@ public class imageConverter {
         Color[][] imageTuple = new Color[image.getWidth()][image.getHeight()]; //create an array with size according to image size
 
         //get image RGB value and store them the array
-        for(int x=0;x<image.getWidth();x++){
-            for (int y=0;y<image.getHeight();y++){
-                imageTuple[x][y] = new Color(image.getRGB(x,y));
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                imageTuple[x][y] = new Color(image.getRGB(x, y));
             }
         }
 
@@ -37,20 +38,25 @@ public class imageConverter {
         int[][] pixelBrightness = new int[image.getWidth()][image.getHeight()];
 
         //get brightness by getting average of RGB Value
-        for (int x = 0;x<image.getWidth();x++){
-            for (int y= 0; y<image.getHeight();y++){
-                pixelBrightness[x][y] = (imageRGBTuple[x][y].getRed()+imageRGBTuple[x][y].getGreen()+imageRGBTuple[x][y].getBlue())/3;
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                pixelBrightness[x][y] = (imageRGBTuple[x][y].getRed() + imageRGBTuple[x][y].getGreen() + imageRGBTuple[x][y].getBlue()) / 3;
             }
         }
         return pixelBrightness;
     }
 
-    public String[][] ASCIIConverter(){
-        String[][] convertedArt=new String[image.getWidth()][image.getHeight()];
+    public String[][] getImageASCIIArt() {
+        String[][] convertedArt = new String[image.getWidth()][image.getHeight()];
         int[][] imageBrightness = getImageBrightness();
-        for (int x = 0; x<image.getWidth();x++){
-            for (int y=0; y<image.getHeight(); y++){
-
+        double divider = (double)ASCII.length/(double)255;;
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                if ((imageBrightness[x][y]*divider)%1>0.5){
+                    convertedArt[x][y] = ASCII[(int) Math.ceil(imageBrightness[x][y]*divider)];
+                }
+                else
+                    convertedArt[x][y] = ASCII[(int) Math.floor(imageBrightness[x][y]*divider)];
             }
         }
 
--- src/main.java
+++ src/main.java
@@ -3,23 +3,16 @@ import java.io.IOException;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        imageConverter test = new imageConverter(".idea//Resources//black_cat_image.jpg");
+        imageConverter test = new imageConverter(".idea//Resources//ascii-pineapple.jpg");
 
         System.out.println("Image Height: " + test.getImageHeight() + " Image Width: " + test.getImageWidth());
-        System.out.println("Image RGB: ");
+        System.out.println("ASCII art: ");
 
-//        Color[][] imageTuple = test.getImageRGBTuple();
-//        for (int y =0;y<test.getImageHeight();y++){
-//            for (int x=0;x<test.getImageWidth();x++){
-//                System.out.print(imageTuple[x][y]);
-//            }
-//            System.out.println();
-//        }
+        String[][] ascii = test.getImageASCIIArt();
 
-        int[][] brightness = test.getImageBrightness();
         for (int y = 0; y < test.getImageHeight(); y++) {
             for (int x = 0; x < test.getImageWidth(); x++) {
-                System.out.print(brightness[x][y]+" ");
+                System.out.print(ascii[x][y]);
             }
             System.out.println();
         }
